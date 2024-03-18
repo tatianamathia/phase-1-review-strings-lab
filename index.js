29278a27366446aefce74af78d47f625192fbfbd
Tatiana Mathia <mathiatatiana@gmail.com>
2024-03-18 22:16:07 +0300

    concatenates strings ,interpolates variables and uses string methods

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+const currentUser = 'Tatiana Mathia' ;
+const welcomeMessage = 'Welcome to Flatbook, ${Tatiana Mathia}!' 
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
